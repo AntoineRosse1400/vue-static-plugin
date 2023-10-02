ec6cabc1526923d0499e2405e60c80f709be599b
Antoine Rossé <antoine.rosse@adonite.com>
2023-10-02 13:50:10 +0200

    Adapt vue-framework-test for export

--- vue-framework-test/src/main.ts
+++ vue-framework-test/src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import Framework from './Framework';
+// import Framework from './Framework';
 
 const app = createApp(App);
-app.use(Framework);
+// app.use(Framework);
 
 app.mount('#app');
--- vue-framework-test/vite.config.ts
+++ vue-framework-test/vite.config.ts
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from 'url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
@@ -24,9 +24,9 @@ export default defineConfig({
   build: {
     cssCodeSplit: false,
     lib: {
-      entry: './src/Framework.ts',
+      entry: './src/FrameworkExporter.ts',
       formats: ['es', 'cjs'],
-      name: 'Framework',
+      name: 'FrameworkExporter',
       fileName: (format) => format === 'es' ? 'index.js' : 'index.cjs'
     },
     rollupOptions: {
